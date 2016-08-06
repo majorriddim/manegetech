8b950e467f4e69a70b7102a79babcfa29cc14a85
majorriddim <majorriddim@gmail.com>
2016-08-06 15:00:19 +0900

    カテゴリー、QAのテーブル作成

---
+++ db/migrate/20160806035031_create_questions.rb
@@ -0,0 +1,11 @@
+class CreateQuestions < ActiveRecord::Migration
+  def change
+    create_table :questions do |t|
+      t.text       :question
+      t.text       :answer
+      t.references :category
+      t.references :question
+      t.timestamps
+    end
+  end
+end
